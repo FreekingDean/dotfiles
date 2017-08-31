ec1f55d671c219728afa5cc0762b2e49a131a6ad
Dean <deangalvin3@gmail.com>
2017-08-31 11:52:33 -0400

    Update 08/31/2017 Deans-MacBook-Pro.local Darwin
    Updated: helper_scripts/git_rm.sh

--- helper_scripts/git_rm.sh
+++ helper_scripts/git_rm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "This will remove: \n"
-git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}'
+`git branch -r | awk '{print $1}' | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk '{print $1}'`
 read -p "Are you sure? (y/n)" -n 1 -r
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
