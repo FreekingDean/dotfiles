70b6288dcbc8592389369875e177981acff7e1e5
Dean <deangalvin3@gmail.com>
2019-09-18 10:06:52 -0400

    Update 09/18/2019 MBP15TB-16-500-XL1DB.local Darwin
    Updated: helper_scripts/gclone.rb

--- helper_scripts/gclone.rb
+++ helper_scripts/gclone.rb
@@ -1,13 +1,17 @@
-repo = ARGV.split("/")
-
 def print_usage
   "gclone repo_name\ngclone user/repo_name"
 end
 
-if repo.count == 0 || repo.count > 2
+if ARGV.count != 1
   print_usage
-if repo.count == 1
-  cmd = "git clone git@github.com:FreekingDean/#{ARGV[0]}.git"
-else
-
+  exit 1
 end
+
+if ARGV[0].include?("/")
+  cmd = "git clone https://github.com/#{ARGV[0]}.git"
+else
+  cmd = "git clone git@github.com:FreekingDean/#{ARGV[0]}.git"
+end
+
+system(cmd)
+exit($?.exitstatus)
