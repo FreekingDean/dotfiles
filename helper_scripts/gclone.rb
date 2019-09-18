67cffbb637fb3817f0c1a52e549b8057f83bad14
Dean <deangalvin3@gmail.com>
2019-09-18 10:05:28 -0400

    Update 09/18/2019 MBP15TB-16-500-XL1DB.local Darwin
    Updated: helper_scripts/.ruby-version
    Updated: helper_scripts/gclone.rb

--- helper_scripts/gclone.rb
+++ helper_scripts/gclone.rb
@@ -1,1 +1,13 @@
-puts ARGV.inspect
+repo = ARGV.split("/")
+
+def print_usage
+  "gclone repo_name\ngclone user/repo_name"
+end
+
+if repo.count == 0 || repo.count > 2
+  print_usage
+if repo.count == 1
+  cmd = "git clone git@github.com:FreekingDean/#{ARGV[0]}.git"
+else
+
+end
