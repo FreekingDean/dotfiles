b3562c8c9faec94a87cda9b4599e44679e137d7d
Dean <deangalvin3@gmail.com>
2020-02-20 22:39:45 -0500

    Update 02/20/2020 MBP15TB-16-500-XL1DB.localdomain Darwin
    Updated: helper_scripts/kubectl.rb

--- helper_scripts/kubectl.rb
+++ helper_scripts/kubectl.rb
@@ -1,2 +1,18 @@
-if ARGV[0] == 'restart'
-end
+require 'json'
+
+override = {
+  spec: {
+    containers: [{
+      volumeMounts: [{
+        mountPath: '/storage',
+        name: 'storage'
+      }]
+    }],
+    volumes: [{
+      name: 'storage',
+      hostPath: '/storage'
+    }]
+  }
+}.to_json
+`KUBECONFIG=~/.kube/config-server kubectl --insecure-skip-tls-verify run -i --tty synstart --image=matrixdotorg/synapse --env="SYNAPSE_SERVER_NAME=riot.public.deangalvin.com" --env="SYNAPSE_REPORT_STATS=yes" --restart=Never --rm --overrides='
+`
